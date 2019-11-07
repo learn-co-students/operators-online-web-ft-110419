3e1a7d01f083ce1b0de849fac4e8c405889f98cc
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-07 16:05:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+if speed > 60 || speed < 40
+  true
+elsif speed == 40 || 60
+  false
+  elsif 
+  true
+  
+end
 end
 
 
-
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ?	true : false
 end
 	
 
